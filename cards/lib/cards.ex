2cb01eae45cf27737e1616e35b82d37cc6c16441
iris <iris.viana@catskillet.com>
2022-02-07 11:35:53 -0300

    elixir warmup

---
+++ cards/lib/cards.ex
@@ -0,0 +1,40 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def create_deck do
+    values = ["one", "two", "three", "four", "five"]
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+    cards = for value <- values, suit <- suits do
+        "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+end
